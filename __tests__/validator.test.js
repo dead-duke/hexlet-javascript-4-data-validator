2174e03f38aad299b8c30982bd222709b30a414b
deus-ex-m <artemterenin@gmail.com>
2023-11-09 00:03:27 +0300

    add number schema tests

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,7 +1,12 @@
 import Validator from '../src/Validator.js';
 
+let validator;
+
+beforeEach(() => {
+  validator = new Validator();
+});
+
 test('string schema', () => {
-  const validator = new Validator();
   const schema = validator.string();
 
   expect(schema.isValid('')).toBeTruthy();
@@ -18,4 +23,24 @@ test('string schema', () => {
   expect(schema.isValid('what does the fox say')).toBeTruthy();
   schema.contains('test');
   expect(schema.isValid('what does the fox say')).toBeFalsy();
+});
+
+test('number schema', () => {
+  const schema = validator.number();
+
+  expect(schema.isValid(null)).toBeTruthy();
+
+  schema.required();
+
+  expect(schema.isValid(null)).toBeFalsy();
+  expect(schema.isValid(7)).toBeTruthy();
+
+  schema.positive();
+
+  expect(schema.isValid(10)).toBeTruthy();
+
+  schema.range(-5, 5);
+
+  expect(schema.isValid(-3)).toBeFalsy();
+  expect(schema.isValid(5)).toBeTruthy();
 });
