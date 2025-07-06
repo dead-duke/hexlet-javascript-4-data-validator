d4dff01aec480ee7caa4d5365045e583a3f15e56
Artem Terenin <artemterenin@gmail.com>
2025-07-06 14:48:53 +0300

    update lint

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,98 +1,103 @@
-import Validator from '../src/Validator.js';
+import Validator from '../src/Validator.js'
+import {
+  test,
+  expect,
+  beforeEach,
+} from '@jest/globals'
 
-let validator;
+let validator
 
 beforeEach(() => {
-  validator = new Validator();
-});
+  validator = new Validator()
+})
 
 test('string schema', () => {
-  const schema = validator.string();
+  const schema = validator.string()
 
-  expect(schema.isValid('')).toBeTruthy();
-  expect(schema.isValid(null)).toBeTruthy();
-  expect(schema.isValid(undefined)).toBeTruthy();
+  expect(schema.isValid('')).toBeTruthy()
+  expect(schema.isValid(null)).toBeTruthy()
+  expect(schema.isValid(undefined)).toBeTruthy()
 
-  schema.required();
+  schema.required()
 
-  expect(schema.isValid('hexlet')).toBeTruthy();
-  expect(schema.isValid(null)).toBeFalsy();
-  expect(schema.isValid('')).toBeFalsy();
+  expect(schema.isValid('hexlet')).toBeTruthy()
+  expect(schema.isValid(null)).toBeFalsy()
+  expect(schema.isValid('')).toBeFalsy()
 
-  schema.contains('what');
-  expect(schema.isValid('what does the fox say')).toBeTruthy();
-  schema.contains('test');
-  expect(schema.isValid('what does the fox say')).toBeFalsy();
-});
+  schema.contains('what')
+  expect(schema.isValid('what does the fox say')).toBeTruthy()
+  schema.contains('test')
+  expect(schema.isValid('what does the fox say')).toBeFalsy()
+})
 
 test('number schema', () => {
-  const schema = validator.number();
+  const schema = validator.number()
 
-  expect(schema.isValid(null)).toBeTruthy();
+  expect(schema.isValid(null)).toBeTruthy()
 
-  schema.required();
+  schema.required()
 
-  expect(schema.isValid(null)).toBeFalsy();
-  expect(schema.isValid(NaN)).toBeFalsy();
-  expect(schema.isValid(7)).toBeTruthy();
+  expect(schema.isValid(null)).toBeFalsy()
+  expect(schema.isValid(NaN)).toBeFalsy()
+  expect(schema.isValid(7)).toBeTruthy()
 
-  schema.positive();
+  schema.positive()
 
-  expect(schema.isValid(10)).toBeTruthy();
+  expect(schema.isValid(10)).toBeTruthy()
 
-  schema.range(-5, 5);
+  schema.range(-5, 5)
 
-  expect(schema.isValid(-3)).toBeFalsy();
-  expect(schema.isValid(5)).toBeTruthy();
-});
+  expect(schema.isValid(-3)).toBeFalsy()
+  expect(schema.isValid(5)).toBeTruthy()
+})
 
 test('array schema', () => {
-  const schema = validator.array();
+  const schema = validator.array()
 
-  expect(schema.isValid(null)).toBeTruthy();
+  expect(schema.isValid(null)).toBeTruthy()
 
-  schema.required();
+  schema.required()
 
-  expect(schema.isValid(null)).toBeFalsy();
-  expect(schema.isValid([])).toBeTruthy();
-  expect(schema.isValid(['hexlet'])).toBeTruthy();
+  expect(schema.isValid(null)).toBeFalsy()
+  expect(schema.isValid([])).toBeTruthy()
+  expect(schema.isValid(['hexlet'])).toBeTruthy()
 
-  schema.sizeof(2);
+  schema.sizeof(2)
 
-  expect(schema.isValid(['hexlet'])).toBeFalsy();
-  expect(schema.isValid(['hexlet', 'code-basics'])).toBeTruthy();
-});
+  expect(schema.isValid(['hexlet'])).toBeFalsy()
+  expect(schema.isValid(['hexlet', 'code-basics'])).toBeTruthy()
+})
 
 test('object schema', () => {
-  const schema = validator.object();
+  const schema = validator.object()
 
-  expect(schema.isValid(null)).toBeTruthy();
-  expect(schema.isValid({})).toBeTruthy();
+  expect(schema.isValid(null)).toBeTruthy()
+  expect(schema.isValid({})).toBeTruthy()
 
   schema.shape({
     name: validator.string().required(),
     age: validator.number().positive(),
-  });
+  })
 
-  expect(schema.isValid()).toBeFalsy();
-  expect(schema.isValid({ name: 'kolya', age: 100 })).toBeTruthy();
-  expect(schema.isValid({ name: 'maya', age: null })).toBeTruthy();
-  expect(schema.isValid({ name: '', age: null })).toBeFalsy();
-  expect(schema.isValid({ name: 'ada', age: -5 })).toBeFalsy();
-});
+  expect(schema.isValid()).toBeFalsy()
+  expect(schema.isValid({ name: 'kolya', age: 100 })).toBeTruthy()
+  expect(schema.isValid({ name: 'maya', age: null })).toBeTruthy()
+  expect(schema.isValid({ name: '', age: null })).toBeFalsy()
+  expect(schema.isValid({ name: 'ada', age: -5 })).toBeFalsy()
+})
 
 test('add user validator', () => {
-  const startsWithFn = (value, start) => value.startsWith(start);
+  const startsWithFn = (value, start) => value.startsWith(start)
 
-  validator.addValidator('string', 'startWith', startsWithFn);
-  const firstSchema = validator.string().test('startWith', 'H');
-  expect(firstSchema.isValid('exlet')).toBeFalsy();
-  expect(firstSchema.isValid('Hexlet')).toBeTruthy();
+  validator.addValidator('string', 'startWith', startsWithFn)
+  const firstSchema = validator.string().test('startWith', 'H')
+  expect(firstSchema.isValid('exlet')).toBeFalsy()
+  expect(firstSchema.isValid('Hexlet')).toBeTruthy()
 
-  const minFn = (value, min) => value >= min;
-  validator.addValidator('number', 'min', minFn);
+  const minFn = (value, min) => value >= min
+  validator.addValidator('number', 'min', minFn)
 
-  const secondSchema = validator.number().test('min', 5);
-  expect(secondSchema.isValid(4)).toBeFalsy();
-  expect(secondSchema.isValid(6)).toBeTruthy();
-});
+  const secondSchema = validator.number().test('min', 5)
+  expect(secondSchema.isValid(4)).toBeFalsy()
+  expect(secondSchema.isValid(6)).toBeTruthy()
+})
--- src/schemas/ArraySchema.js
+++ src/schemas/ArraySchema.js
@@ -1,28 +1,28 @@
 export default class ArraySchema {
   constructor(validators) {
-    this.validators = validators;
-    this.options = {};
+    this.validators = validators
+    this.options = {}
   }
 
   isValid(value) {
-    const validations = Object.values(this.options).map((validator) => validator(value));
-    return !validations.includes(false);
+    const validations = Object.values(this.options).map(validator => validator(value))
+    return !validations.includes(false)
   }
 
   test(name, ...args) {
-    this.options[name] = (value) => this.validators[name](value, ...args);
-    return this;
+    this.options[name] = value => this.validators[name](value, ...args)
+    return this
   }
 
   required() {
-    this.options.required = (value) => (
+    this.options.required = value => (
       value !== null && value !== undefined && Array.isArray(value)
-    );
-    return this;
+    )
+    return this
   }
 
   sizeof(size) {
-    this.options.sizeof = (value) => value.length === size;
-    return this;
+    this.options.sizeof = value => value.length === size
+    return this
   }
 }
